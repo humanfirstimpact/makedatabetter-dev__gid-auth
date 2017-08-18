6472c45c3daacdd26bd520fa0e803691520b84c6
Shreya Bharadwaj <shreyab@nrifintech.com>
2017-08-18 16:18:16 +0530

    Added gh-pages

---
+++ components/gid-api/assets/js/gidRestApi.js
@@ -0,0 +1,20 @@
+var apis = {
+		"lobs": "/lobs",
+		"concepts": "/lobs/{lob}/concepts",
+		"applications": "/concepts/{concept}/applications",
+		"databases": "/concepts/{concept}/applications/{application}/databases",
+		"schemas": "/concepts/{concept}/applications/{application}/databases/{database}/schemas",
+		"tables": "/concepts/{concept}/applications/{application}/databases/{database}/schemas/{schema}/tables",
+		"columns": "/concepts/{concept}/applications/{application}/databases/{database}/schemas/{schema}/tables/{table}/columns",
+		"coverage": "/concepts/{concept}/coverage",
+		"get-samples": "/samples",
+		"recommended-concepts": "/columns/{column}/recommendedconcepts",
+		"verify-concept": "/columns/{column}/recommendedconcepts/{concept}/verify",
+		"list-concepts": "/concepts",
+		"concept-details": "/concepts/{concept}",
+		"curate-concept": "/concepts/{concept}/curate",
+		"create-concept": "/concepts",
+		"edit-concept": "/concepts/{concept}",
+		"remove-concept": "/concepts/{concept}",
+		"auth": "/api/security/api-token-v1"
+	};
